9305d7df5c5c9807519714d6ad472f1684d2c500
Jan Huege <admin@khaostheory.de>
2015-04-27 20:36:30 +0200

    scripts Ordner

---
+++ build/scripts/Article.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Christina on 27.04.2015.
+ */
+define(["require", "exports"], function (require, exports) {
+    var Article = (function () {
+        function Article() {
+        }
+        return Article;
+    })();
+    exports.Article = Article;
+});
---
+++ build/scripts/CustomerController.js
@@ -0,0 +1,27 @@
+if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+define(["require", "exports", 'angular2/angular2', '../CustomerRest'], function (require, exports, angular2_1, CustomerRest_1) {
+    var CustomerController = (function () {
+        function CustomerController() {
+            var CustomerMock = new CustomerRest_1.CustomerREST();
+            this.customer = CustomerMock.getCustomerById(1);
+        }
+        CustomerController = __decorate([
+            angular2_1.Component({
+                selector: 'customercontroller'
+            }),
+            angular2_1.View({
+                template: "\n        <h1>Kundenverwaltung von {{ customer.firstname }} {{ customer.name }}</h1>\n\n    ",
+                directives: [angular2_1.For]
+            })
+        ], CustomerController);
+        return CustomerController;
+    })();
+    angular2_1.bootstrap(CustomerController);
+});
